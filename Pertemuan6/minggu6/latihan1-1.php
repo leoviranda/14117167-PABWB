f2c6d4ac267732aac80f839fc118b5b869e6da62
leoviranda <leo.viranda24@gmail.com>
2019-10-07 11:25:23 +0700

    Latihan 1-6

---
+++ Pertemuan6/minggu6/latihan1-1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Contoh Skrip PHP</title>
+  </head>
+  <body>
+    <?php
+      $nama = "Kim Taehyung";
+      echo "$nama berkata \"save me save me\" ";
+     ?>
+  </body>
+</html>
